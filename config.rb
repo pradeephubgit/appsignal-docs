8acd8b54150933b80b32ae77db018cf5c9945985
Tom de Bruijn <tom@tomdebruijn.com>
2022-02-14 11:59:29 +0100

    Remove TODO about middleman 4.3 upgrade
    We've upgraded to middleman 4.4 in PR #508. This TODO comment can be
    removed now. The warning won't show up anymore.

--- config.rb
+++ config.rb
@@ -1,7 +1,4 @@
 require "lib/appsignal_markdown"
-# TODO: Fixed in middleman 4.3.x. Once it is released we upgrade and remove
-# this silencer.
-Haml::TempleEngine.disable_option_validator!
 
 DOCS_ROOT   = File.expand_path(File.dirname(__FILE__))
 GITHUB_ROOT = "https://github.com/appsignal/appsignal-docs/tree/main"
